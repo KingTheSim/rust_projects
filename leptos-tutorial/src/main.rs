368ceaa0195f1bd76250de02d8daf5b985d66640
nagrevatel-l <KingTheSim@abv.bg>
2024-03-27 00:51:34 +0200

    27_03_2024_very_early_morning

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -2,51 +2,12 @@ use leptos::{ev::MouseEvent, *};
 
 #[component]
 pub fn App() -> impl IntoView {
-    view! {
-        <TakesChildren render_prop=|| view! { <p>"Hi there!"</p> }>
-            "Some text"
-            <span>"A span"</span>
-        </TakesChildren>
-        <WrapsChildren>
-            "A"
-            "B"
-            "C"
-            "D"
-        </WrapsChildren>
-    }
-}
-
-#[component]
-pub fn TakesChildren<F, IV>(
-    /// Takes a function (type F) that returns anything that can be
-    /// converted into a View (type IV)
-    render_prop: F,
-    /// `children` takes the `Children` type
-    children: Children,
-) -> impl IntoView
-where
-    F: Fn() -> IV,
-    IV: IntoView,
-{
-    view! {
-        <h2>"Render Prop"</h2>
-        { render_prop() }
-
-        <h2>"Children"</h2>
-        { children() }
-    }
-}
-
-#[component]
-pub fn WrapsChildren(children: Children) -> impl IntoView {
-    let children = children()
-        .nodes
-        .into_iter()
-        .map(|child| view! { <li>{child}</li> })
-        .collect_view();
-    view! {
-        <ul>{children}</ul>
-    }
+    let (age, set_age) = create_signal(32);
+    let (favorite_number, set_favorite_number) = create_signal(42);
+    let clear_handler = move |_| {
+        set_age(0);
+        set_favorite_number(0);
+    };
 }
 
 fn main() {
